92b646d435729905641380fd0196cde33df67870
Neket <nikita.ziakhor@gmail.com>
2026-02-10 04:06:37 +0000

    make it work idk
    it works now yepyyyy

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -31,17 +32,20 @@ public class Dice : MonoBehaviour
             _delayFinished = true;
             GetNumberOnTopFace();
         }
+    }
 
-
-}
+    // Makes a manu for displaing the top face
     [ContextMenu(itemName:"Get top face")]
+
+    // getting the topface value
     private int GetNumberOnTopFace()
     {
         if (diceFaces == null) return -1;
 
-        var topFace = 5;
-        var lastYPostion = diceFaces[5].position.y;
+        var topFace = 5; // side 6
+        var lastYPostion = diceFaces[5].position.y; 
 
+        // determens the new top face
         for (int i = 0; i < diceFaces.Length; i++)
         {
             if (diceFaces[i].position.y > lastYPostion)
@@ -51,11 +55,32 @@ public class Dice : MonoBehaviour
             }
         }
 
+        // displays the result in the log
         Debug.Log($"Dice result {topFace + 1}");
 
         OndiceResult?.Invoke(_diceIndex, topFace + 1);
-
         return topFace + 1;
+    }
 
+    public void RollDice(float throwForce, float rollforce, int i)
+    {
+        _diceIndex = i;
+        // 
+        var randomVariance = UnityEngine.Random.Range(-1f, 1f);
+        // Impulse gives a little bit of a puch before the engen takes over
+        rb.AddForce(transform.forward * (throwForce + randomVariance), ForceMode.Impulse);
+        
+        var randX = UnityEngine.Random.Range(0f, 1f);
+        var randY = UnityEngine.Random.Range(0f, 1f);
+        var randZ = UnityEngine.Random.Range(0f, 1f);
+
+        rb.AddTorque(new Vector3(randX, randY, randZ) * (rollforce + randomVariance), ForceMode.Impulse);
+        DelayResult();
+    }
+
+    private async Task DelayResult()
+    {
+        await Task.Delay(1000);
+        _delayFinished = true;
     }
 }
--- Assets/Scripts/DiceThrow.cs
+++ Assets/Scripts/DiceThrow.cs
@@ -1,16 +1,41 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class DiceThrow : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Dice diceToThrow;
+    public int amountOfDice = 2;
+    public float throwForce = 5f;
+    public float rollForce = 10f;
+
+    private List<GameObject> _spawnedObjects = new List<GameObject>();
+
+    private void Update()
     {
-        
+        // do func if press space
+        if (Input.GetKeyDown(KeyCode.Space)) RollDice();
     }
 
-    // Update is called once per frame
-    void Update()
+    // async waits defoar spawning a functin
+    private async void RollDice()
     {
-        
+        if(diceToThrow == null) return;
+
+        foreach (var die in _spawnedObjects)
+        {
+            Destroy(die);
+
+        }
+
+        for (int i = 0; i < amountOfDice; i++)
+        {
+            Dice dice = Instantiate(diceToThrow, transform.position, transform.rotation);
+            _spawnedObjects.Add(dice.gameObject);
+            dice.RollDice(throwForce, rollForce, i);
+            await Task.Yield();
+        }
     }
 }
